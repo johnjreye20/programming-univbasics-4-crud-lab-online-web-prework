195f8e03bb197bf0af5c7042ed3c7d31e8ffd4e2
John Reyes <john.jreye@gmail.com>
2019-08-19 01:09:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def digits  = ["one", "two", "three", "four", "five"]
 end
 
 def digits  = ["one", "two", "three", "four", "five"]
-  digits[0]
+  digits[1]
 end
 
 def retrieve_first_element_from_array(array)
